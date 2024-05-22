9b494a37eed9fe294693dfe47a36df82e3c6ada6
relaxcore <yurii.rogulya@teamvoy.com>
2024-05-22 18:30:07 +0300

    Update tests for Application::Event model

--- spec/models/application/event_spec.rb
+++ spec/models/application/event_spec.rb
@@ -6,6 +6,30 @@ RSpec.describe Application::Event, type: :model do
   end
 
   describe 'scopes' do
+    context '#interviews' do
+      it 'returns all events that are interviews' do
+        event1 = create(:application_interview_event)
+        _event2 = create(:application_note_event)
+        _event3 = create(:application_hired_event)
+        _event4 = create(:application_rejected_event)
+        event5 = create(:application_interview_event)
+
+        expect(described_class.interviews).to eq([event1, event5])
+      end
+    end
+
+    context '#notes' do
+      it 'returns all events that are notes' do
+        _event1 = create(:application_interview_event)
+        event2 = create(:application_note_event)
+        _event3 = create(:application_hired_event)
+        _event4 = create(:application_rejected_event)
+        event5 = create(:application_note_event)
+
+        expect(described_class.notes).to eq([event2, event5])
+      end
+    end
+
     context '#excluding_notes' do
       it 'returns all events excluding notes' do
         event1 = create(:application_interview_event)
